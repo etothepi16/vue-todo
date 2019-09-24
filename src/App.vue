06a80f90655ad93ced23c0435e41f9bc7e3dcff9
etothepi16 <paulmcdowell18@gmail.com>
2019-09-23 22:22:33 -0400

    Removed the Extraneous Component from Hell. Good riddance.

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,17 @@
   >
     <Header />
     <Sidebar v-if="user" />
-
-    <!-- TODO: MAKE MOBILE RESPONSIVE -->
     <router-view></router-view>
-
-    <!-- <SidebarRight v-if="user" /> -->
   </v-app>
 </template>
 
 <script>
 import Sidebar from "./components/layout/Sidebar";
-import SidebarRight from "./components/layout/SidebarRight";
 import Header from "./components/layout/Header";
 export default {
   name: "app",
   components: {
     Sidebar,
-    SidebarRight,
     Header
   },
   computed: {
